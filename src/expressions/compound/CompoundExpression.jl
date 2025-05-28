5dde0c8077544a7e6ab0bc8895ecc875f3e59804
Ouroboros42 <lucas.gathercole@gmail.com>
2025-05-28 17:22:13 +0100

    Avoid converting large lists to tuples in unionise

--- src/expressions/compound/CompoundExpression.jl
+++ src/expressions/compound/CompoundExpression.jl
@@ -10,10 +10,14 @@ isless(first::C, second::C) where {C <: CompoundExpression} = isless(args(first)
 
 const NyTuple{T} = NTuple{N, T} where N
 
+typeunion(T1, T2) = Union{T1, T2}
+
 unionise_recursive(type, ::Tuple{}) = type
 unionise_recursive(type, (expr, others...)::NyTuple{Expression}) = unionise_recursive(Union{type, valuetype(expr)}, others)
 
 unionise(expressions::NyTuple{Expression}) = unionise_recursive(Union{}, expressions)
 unionise(::NyTuple{Expression{T}}) where T = T
-unionise(expressions::Vector{E}) where {E <: Expression} = unionise(Tuple(expressions))
+unionise(expressions::Expression...) = unionise(expressions)
+
+unionise(expressions::Vector{E}) where {E <: Expression} = mapreduce(valuetype, typeunion, expressions)
 unionise(::Vector{E}) where {T, E <: Expression{T}} = T
