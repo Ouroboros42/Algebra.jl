de05635a61ba6ab99cb1256f0cf0c3dcc482e3b1
Ouroboros42 <lucas.gathercole@gmail.com>
2025-07-08 03:25:57 +0100

    Make Literal ordering independent of value type

--- src/expressions/Literal.jl
+++ src/expressions/Literal.jl
@@ -7,6 +7,9 @@ end
 value(literal::Literal) = literal.value
 
 isequal(first::Literal, second::Literal) = first.value == second.value
+
+isless(::Expression, ::Literal) = false
+isless(::Literal, ::Expression) = true
 isless(first::Literal, second::Literal) = isless(first.value, second.value)
 
 print(io::IO, literal::Literal) = print(io, literal.value)
