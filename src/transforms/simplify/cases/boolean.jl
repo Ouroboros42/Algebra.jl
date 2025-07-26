4f2b5bd7ab8e1275a40032456fa6746d62d89e33
Ouroboros42 <lucas.gathercole@gmail.com>
2025-07-26 01:59:33 +0100

    Handle And and Or simple tautologies

--- src/transforms/simplify/cases/boolean.jl
+++ src/transforms/simplify/cases/boolean.jl
@@ -3,6 +3,20 @@ tryevaluate(::Trivial, ::Type{<:Not}, a::Bool) = !a
 
 trycombine(::Trivial, ::Type{<:Not}, not::Not) = arg(not)
 
+areconverse(simplifier::Simplifier, a::Expression{Bool}, b::Expression{Bool}) = isequal(apply(simplifier, !a), b)
+
+function trycombine(simplifier::Simplifier, ::Type{<:And}, a::Expression{Bool}, b::Expression{Bool})
+    if isequal(a, b); return a end
+
+    if areconverse(simplifier, a, b); return FALSE end
+end
+
+function trycombine(simplifier::Simplifier, ::Type{<:Or}, a::Expression{Bool}, b::Expression{Bool})
+    if isequal(a, b); return a end
+
+    if areconverse(simplifier, a, b); return TRUE end
+end
+
 function tryapply(::Trivial, compound::Compound)
     if compound isa IfElse; return end
 
@@ -11,4 +25,8 @@ function tryapply(::Trivial, compound::Compound)
             replacesomeargs(compound, i => branch)
         end 
     end
+end
+
+function tryapply(::Trivial, conditional::IfElse{Bool})
+    (condition(conditional) & truebranch(conditional)) | (!condition(conditional) & falsebranch(conditional))
 end
