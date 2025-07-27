b814ef1d8b36dbf23dcc8d3168c20aad434c9f32
Ouroboros42 <lucas.gathercole@gmail.com>
2025-07-27 18:25:25 +0100

    Remove brackets from Not print

--- src/expressions/compound/operation/ops/Not.jl
+++ src/expressions/compound/operation/ops/Not.jl
@@ -1,6 +1,6 @@
 const Not = @operator Operation{!, 1}
 
-print(io::IO, not::Not) = print(io, "!($(arg(not)))")
+print(io::IO, not::Not) = print(io, "!$(arg(not))")
 
 tryinfervaltype(::Type{<:Not}, ::Type{Bool}) = Bool
 
