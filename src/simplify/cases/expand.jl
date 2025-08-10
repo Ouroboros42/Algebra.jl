3275dc94e96616aa1981a06829468b347ae2a2f6
Ouroboros42 <lucas.gathercole@gmail.com>
2025-08-10 23:19:50 +0100

    Generalise expanding products

--- src/simplify/cases/expand.jl
+++
@@ -1,5 +0,0 @@
-function trycombine(simplifier, ::Type{<:Prod}, sum1::Sum, sum2::Sum)
-    if expands(simplifier)
-        Sum(map(Prod, Iterators.product(args(sum1), args(sum2))))
-    end
-end
--- src/simplify/cases/multiples.jl
+++ src/simplify/cases/multiples.jl
@@ -2,10 +2,28 @@ repeated_op(::Type{<:Associative}) = nothing
 repeated_op(::Type{<:Sum}) = Prod
 repeated_op(::Type{<:Prod}) = Pow 
 
+tryexpand(::Expression) = nothing
+expand(expr::Expression) = @ordefault(tryexpand(expr), [expr])
+
+expandprod(factors) = vec(map(Prod, Iterators.product(map(toargs(Sum), factors)...)))
+tryexpand(product::Prod) = if any(isinst(Sum), args(product))
+    expandprod(args(product))
+end
+tryexpand((base, exponent)::Pow) = if base isa Sum && exponent isa Literal{<:Integer} && ispositive(exponent)
+    expandprod(ntuple(_ -> args(base), value(exponent)))
+end
+
 function trycombine(simplifier, outer::Type{<:Associative}, expr1::Expression, expr2::Expression)
-    mapsome(repeated_op(outer)) do repeated
+    @tryreturn mapsome(repeated_op(outer)) do repeated
         if isvalid(repeated, expr1, TWO) && isequal(expr1, expr2)
-            return repeated(expr1, TWO)
+            repeated(expr1, TWO)
+        end
+    end
+
+    if outer <: Sum
+        expanded = [expand(expr1); expand(expr2)]
+        if length(expanded) > 2
+            return Sum(expanded)
         end
     end
 end
